7b50fddaa9b08adf94de08535393789f16dfde0c
hslape <136274044+hslape@users.noreply.github.com>
2023-09-13 11:14:13 -0500

    Add files via upload
    exercise 7 & 8 completed

--- class-exercises.js
+++ class-exercises.js
@@ -159,3 +159,114 @@ person6.greet();
 // It returned "Age cannot be negative." in the console, but it also returned the object with the age property set to undefined.
 
 // Exercise 5 complete
+
+
+// Exercise 6
+
+// Objective: Understand and implement private fields in classes.
+// Instructions:
+// 1. Visit a linked repository.
+// 2. Click Use this template followed by Create new repository.
+// 3. Give this repository a name of private fields in classes.
+// 4. Make sure this is a public repository.
+// 5. Read the readMe.md file for further instructions.
+
+// Exercise 6 to be completed in separate repository
+
+
+// Exercise 7
+
+// Objective: Integrate multiple concepts: classes, methods, and private fields.
+
+// Instructions:
+// 1. As a challenge, design a Library class. This class should be able to store book objects.
+// 2. For book objects, you should have a Book class. This class should have private fields for ISBN and public fields like title, author, and yearPublished.
+// 3. The Library should feature methods that allow a user to add a book, remove a book, and list all books.
+// 4. Implement this system and run a series of operations, such as adding a book, listing books, etc.
+
+
+class Library {
+    constructor() {
+        this.books = [];
+    }
+    addBook(book) {
+        this.books.push(book);
+    }
+    removeBook(book) {
+        this.books.splice(this.books.indexOf(book), 1);
+    }
+    listBooks() {
+        console.log(this.books);
+    }
+}
+
+class Book {
+    #isbn;
+    constructor(title, author, yearPublished, isbn) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.#isbn = isbn;
+    }
+}
+
+const library = new Library();
+
+const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 1937, 9780261103283);
+const book2 = new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', 1954, 9780261102354);
+const book3 = new Book('The Two Towers', 'J.R.R. Tolkien', 1954, 9780261102361);
+const book4 = new Book('The Return of the King', 'J.R.R. Tolkien', 1955, 9780261102378);
+
+library.addBook(book1);
+library.addBook(book2);
+library.addBook(book3);
+library.addBook(book4);
+
+library.listBooks();
+
+library.removeBook(book3);
+
+// Exercise 7 complete
+
+
+// Exercise 8
+
+// Instructions:
+// Create a class Student with properties name and grades. Implement methods to add grades and compute the average. How does the this keyword behave differently in arrow funcitons compared to regular funcitons?
+
+class Student {
+    constructor (name, grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    addGrades(grade) {
+        this.grades.push(grade);
+    }
+
+    computeAverage() {
+        let sum = 0;
+        for (let i = 0; i < this.grades.length; i++) {
+            sum += this.grades[i];
+        }
+        return sum / this.grades.length;
+    }
+
+    computeAverageArrow = () => {
+        let sum = 0;
+        for (let i = 0; i < this.grades.length; i++) {
+            sum += this.grades[i];
+        }
+        return sum / this.grades.length;
+    }
+}
+
+const student1 = new Student('Valory', [100, 95, 90, 85, 80]);
+const student2 = new Student('Zane', [95, 90, 90, 85, 80]);
+
+console.log(student1.computeAverage());
+console.log(student2.computeAverageArrow());
+
+// The this keyword behaves the same in both regular functions and arrow functions.
+
+// Exercise 8 complete
